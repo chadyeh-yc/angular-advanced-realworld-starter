cf8ea93d4121599edf94229912f53130750f632b
葉永青 Chad Yeh <chad.yeh@miniasp.com>
2020-09-06 14:54:56 +0800

    任務 04：在程式中動態切換路由

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,11 @@
+import { UserLoginInfo } from './../interfaces/login-info';
+import { LoginService } from './../login.service';
+import { Routes, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: UserLoginInfo = {
+    email: '',
+    password: ''
+  };
 
-  ngOnInit(): void {
+  constructor(private router: Router, private loginService: LoginService) { }
+
+  ngOnInit(): void {  }
+
+  login(): void {
+    this.loginService.login(this.user).pipe(
+      catchError((error: HttpErrorResponse) => {
+        alert(error.message);
+        return throwError(error);
+      })
+    ).subscribe(result => {
+      localStorage.setItem('token', result.user.token);
+      this.router.navigateByUrl('/');
+    });
   }
 
 }
